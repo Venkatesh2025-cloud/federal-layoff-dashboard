b3277a9177d14a71995f945b36ad48c24b0a62e4
Venkatesh2025-cloud <venkatvj1996@gmail.com>
2025-04-23 17:37:39 +0530

    Update federal_layoffs_dashboard.py

--- dashboard/federal_layoffs_dashboard.py
+++ dashboard/federal_layoffs_dashboard.py
@@ -8,9 +8,12 @@ import altair as alt
 import os
 
 st.set_page_config(
+    page_title='🧠 Federal Layoffs & Skills Intelligence',
+    layout='wide'
+)
 
 # === Custom CSS Loader ===
-def inject_custom_css(file_path="dashboard/streamlit_dashboard_custom_style.css"):
+def inject_custom_css(file_path="streamlit_dashboard_custom_style.css"):
     try:
         with open(file_path) as f:
             st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
